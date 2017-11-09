740f085cc56e3ec2b43cb9b99e8deb2a3b0e8a8d
Michael Toomim <toomim@gm.com>
2017-11-09 00:04:23 -0800

    Fix race condition with save/delete in sqlite_store

--- server.js
+++ server.js
@@ -619,7 +619,7 @@ function import_server (bus, options)
         }
 
         // Add save handlers
-        function on_save (obj) {
+        function store_it (obj) {
             if (global.pointerify)
                 obj = abstract_pointers(obj)
 
@@ -642,8 +642,11 @@ function import_server (bus, options)
             }
 
         }
-        on_save.priority = true
-        bus(prefix).on_save = on_save
+        var old_route = bus.route
+        bus.route = function (key, method, arg, t) {
+            if (method === 'to_save') store_it(arg)
+            return old_route(key, method, arg, t)
+        }
         bus(prefix).to_delete = function (key) {
             if (opts.use_transactions && !open_transaction){
                 console.time('save db')
